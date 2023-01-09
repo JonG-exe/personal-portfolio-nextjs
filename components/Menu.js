4c405977517092e1f6dcde2a529ff53a11468d68
Jonathan Gray <zeusdcs@gmail.com>
2023-01-09 05:00:33 -0400

    Turned off SEO link in menu_modal

--- components/Menu.js
+++ components/Menu.js
@@ -6,7 +6,10 @@ function Menu (props) {
 
     return(
         <div className="menu_modal" style={{"display" : ( props.menuActive ? "flex" : "none" )}}>
-            <Link href='/SEO' className="modal_link">SEO Page</Link>
+            {/* <Link href='/SEO' className="modal_link">SEO Page</Link> */}
+
+            <p>Coming Soon!</p>
+            
             </div>
     )
 }
--- pages/SEO.js
+++ pages/SEO.js
@@ -1,7 +1,5 @@
 import Tutorial from "../components/Tutorial"
 import ImageAuthor from "../images/23.png"
-
-
 import tempProfilePhoto from "../images/jon-g-profile-picture.png"
 
 export default function SEO() {
@@ -9,7 +7,7 @@ export default function SEO() {
         <Tutorial 
             mainContent={
                 <div>
-                    <h1>MAN JS IS FUCKING AMAZING!</h1>
+                    {/* <h1>MAN JS IS FUCKING AMAZING!</h1>
 
                     <img className="tutorial-content-img" src={tempProfilePhoto.src} alt="author's profile photo"/>
 
@@ -17,12 +15,12 @@ export default function SEO() {
 
                     <p>A testament to the north gods of euroslovakia!</p>
 
-                    <br></br>
+                    <br></br> */}
                 </div>
             }
 
             authorImg={ImageAuthor}
-            authorName="Jon.H"
+            authorName="Jon.G"
         />
     )
 }
