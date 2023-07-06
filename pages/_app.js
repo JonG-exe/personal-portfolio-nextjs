75212337751341a2d2f5ed72049a686d47f832e9
Jonathan Gray <zeusdcs@gmail.com>
2023-07-06 13:01:31 -0400

    Added Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import Head from "next/head"
 import "../styles/index.css"
 import "../styles/contact.css"
 import "../styles/services.css"
@@ -6,10 +5,25 @@ import "../styles/tutorial.css"
 import Navbar from "../components/Navbar"
 import { Analytics } from "@vercel/analytics/react"
 import Footer from "../components/Footer"
+import Head from "next/head"
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className="app">
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-KQJY74WVM2"></Script>
+      <Script>
+        {
+          `
+            <!-- Google tag (gtag.js) -->
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-KQJY74WVM2');
+          `
+        }
+      </Script>
       <Navbar />
 
       <Component {...pageProps} />
